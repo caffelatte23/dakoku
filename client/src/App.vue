edab9c6707cc4bccae71dfc811b1c7d3c5796bf2
caffelatte23 <caffelatte623@gmail.com>
2022-10-29 16:15:00 +0900

    client: mount時に勤務状態を取得するように修正

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,6 +1,30 @@
 <script setup lang="ts">
 import axios from "axios";
 import dayjs from "dayjs";
+import { onMounted, ref } from "vue";
+
+type status = "NOT_WORKING" | "WORKING";
+
+interface workingStatus {
+  userId: string;
+  date: string;
+  startTime: string;
+  endTime: string;
+  status: status;
+}
+
+const currentStatus = ref<workingStatus>({
+  userId: "1",
+  date: dayjs().format("YYYY-MM-DD"),
+  startTime: "",
+  endTime: "",
+  status: "NOT_WORKING",
+});
+
+const Status: { NOT_WORKING: string; WORKING: string } = {
+  NOT_WORKING: "勤務時間外",
+  WORKING: "勤務中",
+};
 
 // 出勤時
 const onAttend = (): void => {
@@ -18,14 +42,29 @@ const onLeave = (): void => {
   axios.post(import.meta.env.VITE_API_ROOT, {
     userId: "1",
     time: dayjs().format("YYYY-MM-DD HH:mm"),
-    IsAttend: true,
+    IsAttend: false,
   });
 };
+
+const refreshStatus = (): void => {
+  axios
+    .get(import.meta.env.VITE_API_ROOT, {
+      params: {
+        userId: "1",
+        date: dayjs().format("YYYY-MM-DD"),
+      },
+    })
+    .then((res) => {
+      currentStatus.value = res.data;
+    });
+};
+
+onMounted((): void => refreshStatus());
 </script>
 
 <template>
   <div class="body">
-    <span>現在のステータス: 勤務時間外</span>
+    <span>現在のステータス: {{ Status[currentStatus.status] }}</span>
     <div class="button-container">
       <button class="button" @click="onAttend">出勤</button>
       <button class="button" @click="onLeave">退勤</button>
